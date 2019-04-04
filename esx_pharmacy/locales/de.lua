d58b41660515b2afe8efedbaebc26cb23f733430
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-04 08:49:17 +0200

    Update and rename fr.lua to de.lua

---
+++ esx_pharmacy/locales/de.lua
@@ -0,0 +1,30 @@
+Locales['de'] = {
+
+    ['pharma-kit']         	= 'Erste-Hilfe-Ausrüstung',
+    ['pharma-defi']      	= 'Defibrillator',
+
+    ['yes']                 = 'Ja',
+    ['no']                  = 'Nein',
+    ['press_menu']          = 'Benutze ~INPUT_CONTEXT~ auf die Apotheke zuzugreifen.',
+    ['drugstore']           = 'Apotheke',
+    ['valid_this_purchase'] = 'Bestätige Deinen Kauf.',
+    ['not_enough_money']    = 'Du hast nicht genug Geld!',
+    ['bought']              = 'Gekauft: ~g~',
+    ['use_pharma-kit']     	= 'Du benutztest ein ~g~Erste-Hilfe-Kasten',
+    ['has_ambulance']       = '~r~Du kannst dieses Notfallset nur nutzen, wenn kein Sanitäter in der Stadt ist!',
+
+    ['no_players']          = 'Kein Anwohner in der Nähe!',
+    ['revive_inprogress']   = 'Reanimation in Arbeit...',
+    ['revive_complete']     = 'Reanimation erfolgreich!',
+    ['isdead']              = 'Leider verstorben!',
+    ['unconscious']         = 'Ist nicht bewusstlos!'
+
+}
+
+-- Translation by
+-- NebelRebell
+-- Nebelbank.net
+-- Power of Perfection
+-- Don't flame without brain!
+
+-- 've a nice day!
