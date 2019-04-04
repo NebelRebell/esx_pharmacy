2c2d358dbfdd448dfe16c8c8980d9ee373d96be6
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-04-04 08:45:33 +0200

    Update __resource.lua

--- esx_pharmacy/__resource.lua
+++ esx_pharmacy/__resource.lua
@@ -1,10 +1,15 @@
-description 'Pharmacie'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'esx_pharmacy'
+
+version '1.1.NB'
+
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     '@es_extended/locale.lua',
     'server/esx_pharmacy.server.lua',
-    'locales/fr.lua',
+    'locales/de.lua',
     'config.lua'
 }
 
@@ -12,6 +17,10 @@ client_scripts {
     '@es_extended/locale.lua',
     'client/esx_pharmacy.client.lua',
     'client/esx_pharmacy_gui.client.lua',
-    'locales/fr.lua',
+    'locales/de.lua',
     'config.lua'
+}
+
+dependencies {
+	'es_extended'
 }
